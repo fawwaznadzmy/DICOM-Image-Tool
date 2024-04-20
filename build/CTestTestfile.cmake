d44d04e34aacaca32749a3b8d254d5e4f212a5eb
Fawwaz Nadzmy <fawwaz.nadzmy@gmail.com>
2024-04-20 23:13:32 +0800

    Add build folder

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Project/Git/DICOM_V2
+# Build directory: D:/Project/Git/DICOM_V2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

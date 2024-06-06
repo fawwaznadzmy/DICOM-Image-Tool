31ef9392ed9bd01fee5e6273e2cb87a65a54f01e
Fawwaz Nadzmy <fawwaz.nadzmy@gmail.com>
2024-06-06 11:59:41 +0800

    remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project/Git/DICOM_V2/DICOM-Image-Tool
-# Build directory: D:/Project/Git/DICOM_V2/DICOM-Image-Tool/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("app")
-subdirs("test")
--- build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project/Git/DICOM_V2/DICOM-Image-Tool/app
-# Build directory: D:/Project/Git/DICOM_V2/DICOM-Image-Tool/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
